70e47baae7105fa1424a2f7eda0ae46abf9dd851
zhang <jisong.zhang@vixtel.com>
2019-04-22 14:51:17 +0800

    dev

---
+++ app/src/main/java/com/jeasoon/intent/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jeasoon.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
